4f9cbeae61181508406c60426b675b4239259482
Altynai <altynai.sabetova@gmail.com>
2019-04-17 02:36:58 +0600

    change html and css

--- download/models.py
+++ download/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 
 class Data(models.Model):
-    data_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date')
     def __str__(self):
         return self.data_name
--- download/views.py
+++ download/views.py
@@ -4,7 +4,6 @@ from .forms import Download
 import youtube_dl
 from .models import Data
 
-
 def field(request):
     if request.method == 'POST':
         form = Download(request.POST)
@@ -25,20 +24,3 @@ def field(request):
         form = Download()
 
     return render(request, 'field.html', {'form': form})
-
-
-
-"""\    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-    with ydl:
-        result = ydl.extract_info(
-        'http://www.youtube.com/watch?v=BaW_jenozKc',
-        download=False # We just want to extract the info
-        )
-        if 'entries' in result:
-            video = result['entries'][0]
-        else:
-            video = result
-            print(video)
-            video_url = video['link']
-            print(video_url)
-"""
