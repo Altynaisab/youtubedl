bf0c9ca95f178ceed78281e041a5cb46c021df71
Altynai <altynai.sabetova@gmail.com>
2019-04-21 20:14:49 +0600

    update histiry

--- download/views.py
+++ download/views.py
@@ -6,22 +6,29 @@ import youtube_dl
 from .models import Data
 from django.utils.timezone import now
 
+def getaudio(temp):
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [
+    {'key': 'FFmpegExtractAudio','preferredcodec': 'mp3',
+     'preferredquality': '192',
+    },
+    {'key': 'FFmpegMetadata'},
+        ],
+                }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        meta = ydl.extract_info(temp)
+
+    Data.objects.create(name=meta['title'], pub_date = now())
+    return meta['title']
+
+
 def field(request):
     if request.method == 'POST':
         form = Download(request.POST)
         if form.is_valid():
-            link = form.cleaned_data
-            ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [
-            {'key': 'FFmpegExtractAudio','preferredcodec': 'mp3',
-             'preferredquality': '192',
-            },
-            {'key': 'FFmpegMetadata'},
-                ],
-                        }
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([link['link']])
+            temp = form.cleaned_data['link']
+            x = getaudio(temp)
 
     else:
         form = Download()
@@ -29,4 +36,4 @@ def field(request):
     return render(request, 'field.html', {'form': form})
 
 def history(request):
-    return render(request, 'history.html', {'options' : Data.objects.all()})
+    return render(request, 'history.html', {'options': Data.objects.all()})
